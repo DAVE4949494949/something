3fe1efc24f7d946053dd6a191798cdcb3cb89819
Dmitry Maklygin <d.maklygin@gmail.com>
2015-03-25 16:09:59 +0300

    Created new action

--- actions/updateCategories.js
+++ actions/updateCategories.js
@@ -1,3 +1,18 @@
-/**
- * Created by dmitrymaklygin on 25.03.15.
- */
+exports.action = {
+  name:                   'updateCategories',
+  description:            'update only categories',
+  blockedConnectionTypes: [],
+  outputExample:          {},
+  matchExtensionMimeType: false,
+  version:                1.0,
+
+  inputs: {
+    required: [ 'id' ],
+    optional: []
+  },
+
+  run: function(api, connection, next){
+    console.log('run update Categories');
+    next(connection, true);
+  }
+};
